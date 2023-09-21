c3f3ef972b7ea80c503e613e21cefab79e7d8e6a
GHEZIL Achref <achref.ghezil@talan.com>
2023-09-21 11:01:40 +0200

    Commit init

---
+++ src/multimedia/Histogramme.java
@@ -0,0 +1,68 @@
+package multimedia;
+
+import java.util.ArrayList;
+
+/**
+ * Ajouter en commentaire la description de la classe.
+ *
+ * @version $Revision: $ - $Date: $
+ * @author $LastChangedBy: $
+ * <pre>
+ * $URL: $
+ * </pre>
+ *
+ */
+public class Histogramme {
+	private int[] red_histogramme ;
+	private int[] green_histogramme ;
+	private int[] blue_histogramme ;
+	/**
+	 * @param hr
+	 * @param hb
+	 * @param hg
+	 */
+	public Histogramme(int[] hr, int[] hb, int[] hg) {
+		super();
+		this.red_histogramme = hr;
+		this.green_histogramme = hb;
+		this.blue_histogramme = hg;
+	}
+	
+	/**
+	 * @return the red_histogramme
+	 */
+	public int[] getRed_histogramme() {
+		return red_histogramme;
+	}
+	/**
+	 * @param red_histogramme the red_histogramme to set
+	 */
+	public void setRed_histogramme(int[] red_histogramme) {
+		this.red_histogramme = red_histogramme;
+	}
+	/**
+	 * @return the green_histogramme
+	 */
+	public int[] getGreen_histogramme() {
+		return green_histogramme;
+	}
+	/**
+	 * @param green_histogramme the green_histogramme to set
+	 */
+	public void setGreen_histogramme(int[] green_histogramme) {
+		this.green_histogramme = green_histogramme;
+	}
+	/**
+	 * @return the blue_histogramme
+	 */
+	public int[] getBlue_histogramme() {
+		return blue_histogramme;
+	}
+	/**
+	 * @param blue_histogramme the blue_histogramme to set
+	 */
+	public void setBlue_histogramme(int[] blue_histogramme) {
+		this.blue_histogramme = blue_histogramme;
+	}
+
+}
---
+++ src/multimedia/Multimedia.java
@@ -0,0 +1,97 @@
+package multimedia;
+
+/**
+ * Ajouter en commentaire la description de la classe.
+ *
+ * @version $Revision: $ - $Date: $
+ * @author $LastChangedBy: $
+ * <pre>
+ * $URL: $
+ * </pre>
+ *
+ */
+public class Multimedia {
+	private int id;
+	private double moynormegradiant;
+	private Histogramme histogramme ;
+	private boolean isNb ;
+	private int distance ;//taux de resemblance 
+	public Multimedia(int id, double moynormegradiant, Histogramme histogramme, boolean isNb) {
+		super();
+		this.id = id;
+		this.moynormegradiant = moynormegradiant;
+		this.histogramme = histogramme;
+		this.isNb = isNb;
+		this.distance = 0;
+	}
+	/**
+	 * @return the id
+	 */
+	public int getID() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setID(int id) {
+		this.id = id;
+	}
+	/**
+	 * @return the moynormegradiant
+	 */
+	public double getMoynormegradiant() {
+		return moynormegradiant;
+	}
+	/**
+	 * @param moynormegradiant the moynormegradiant to set
+	 */
+	public void setMoynormegradiant(double moynormegradiant) {
+		this.moynormegradiant = moynormegradiant;
+	}
+	/**
+	 * @return the histogramme
+	 */
+	public Histogramme getHistogramme() {
+		return histogramme;
+	}
+	/**
+	 * @param histogramme the histogramme to set
+	 */
+	public void setHistogramme(Histogramme histogramme) {
+		this.histogramme = histogramme;
+	}
+	/**
+	 * @return the isNb
+	 */
+	public boolean isNb() {
+		return isNb;
+	}
+	/**
+	 * @param isNb the isNb to set
+	 */
+	public void setNb(boolean isNb) {
+		this.isNb = isNb;
+	}
+	/**
+	 * @param id
+	 * @param moynormegradiant
+	 * @param histogramme
+	 * @param isNb
+	 */
+	/**
+	 * @return the distance
+	 */
+	public int getDistance() {
+		return distance;
+	}
+	/**
+	 * @param distance the distance to set
+	 */
+	public void setDistance(int distance) {
+		this.distance = distance;
+	}
+
+	
+
+
+}
